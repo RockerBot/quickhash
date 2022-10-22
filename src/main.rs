09f052f918c82f6778f0c61e1c8496b136b4e1dd
abhiraj2 <abhiraj.official15@gmail.com>
2022-10-22 10:56:31 +0530

    fixed_sha1_final

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ fn main() -> std::io::Result<()> {
     // Makes a selection menu in the terminal with the available algs
     let selection = Select::with_theme(&ColorfulTheme::default())
         .items(&hash_algorithms)
-        .default(2)
+        .default(0)
         .interact_on_opt(&Term::stderr())?;
 
     match selection {
